35e7c4de5c9e2cff6dec24e3417f29e67e8d0f1e
kakeyuki <nieve.brillantemente@gmail.com>
2024-05-31 21:12:12 +0800

    JavaScriptの基礎を学ぼう　21章課題をPUSHしました

---
+++ kadai_021/event.js
@@ -0,0 +1,15 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const change = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し定数に代入する
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+  // textにテキストを追加する
+  text.textContent = 'ボタンをクリックしました';
+  // 作成したテキストを出力する
+  console.log(text);
+}, 2000);
+});
