5d357eb77b8e684a35149912ffed423b1c9a310d
kakeyuki <nieve.brillantemente@gmail.com>
2024-05-31 21:34:01 +0800

    JavaScriptの基礎を　21章課題を再提出いたします

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,5 +1,5 @@
 // btnというidを持つHTML要素を取得し、定数に代入する
-const change = document.getElementById('btn');
+const btn = document.getElementById('btn');
 
 // textというidを持つHTML要素を取得し定数に代入する
 const text = document.getElementById('text');
